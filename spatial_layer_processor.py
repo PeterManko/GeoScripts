430255372f27e3327b7440c407b7961f2f5c55c6
= <=>
2024-07-19 23:53:11 +0200

    file extensions are now case insensitive

--- spatial_layer_processor.py
+++ spatial_layer_processor.py
@@ -28,7 +28,7 @@ def list_files(directory):
     file_list = []
     for root, dirs, files in os.walk(directory):
         for file in files:
-            if file.endswith('.geojson'):
+            if file.lower().endswith('.geojson'):
                 file_path = os.path.join(root, file)
                 file_list.append(file_path)
     return file_list
